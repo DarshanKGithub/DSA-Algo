d868307156c9c5cb8c0300770de60b7f02b2efde
DarshanKGithub <darshankshetri8@gmail.com>
2025-09-30 23:42:27 +0530

    Selection Sort

---
+++ SelectionSort.java
@@ -0,0 +1,43 @@
+public class SelectionSort {
+    // Function to perform Selection Sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            // Assume the current index has the minimum
+            int minIndex = i;
+
+            // Find the actual minimum in remaining array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap if a smaller element was found
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Utility function to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+}
